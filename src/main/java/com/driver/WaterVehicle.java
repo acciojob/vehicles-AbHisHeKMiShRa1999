3a987225f9a76fef87852b4cfd34575da530a59f
abhishekmishra <am86373@gmail.com>
2023-01-21 21:50:37 +0530

    first

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,3 +4,21 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
+class Boat implements WaterVehicle{
+    private String name;
+    private int capacity;
+    public Boat(String name,int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    @java.lang.Override
+    public String getVehicleName() {
+        return this.name;
+    }
+
+    @java.lang.Override
+    public int getVehicleCapacity() {
+        return this.capacity;
+    }
+}
