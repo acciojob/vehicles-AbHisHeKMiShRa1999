fe2318f64a8eaa8c911342e84e2e7f452b76ed03
AbHisHeKMiShRa1999 <am86373@gmail.com>
2023-01-27 22:16:49 +0530

    second

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,54 +1,43 @@
 package com.driver;
-
 public class F1 extends Car {
-
     public F1(String name, boolean isManual) {
-        //Use arbitrary values for parameters which are not mentioned
-        super(name,0,0,0,isManual,null,1);
+        super(name, 0, 0, 0, isManual, null, 1);
+//Use arbitrary values for parameters which are not mentioned
     }
 
-    public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-        newSpeed += this.getCurrentSpeed()+ rate;
-        /**
-         * speed 0: gear 1
-         * speed 1-50: gear 1
-         * speed 51-100: gear 2
-         * speed 101-150: gear 3
-         * speed 151-200: gear 4
-         * speed 201-250: gear 5
-         * speed more than 250: gear 6
-         */
+    public void accelerate(int rate) {
+        int newSpeed = 0; //set the value of new speed by using currentSpeed
 
-        if(newSpeed == 0) {
-            this.setGears(1);
+        newSpeed += this.getCurrentSpeed() + rate;
+/**
+ * speed 0: gear 1
+ * speed 1-50: gear 1
+ * speed 51-100: gear 2
+ * speed 101-150: gear 3
+ * speed 151-200: gear 4
+ * speed 201-250: gear 5
+ * speed more than 250: gear 6
+ */
+        if (newSpeed == 0) {
             //Stop the car, set gear as 1
+            this.setGears(1);
         }
-
-        //for all other cases, change the gear accordingly
-
-        if(newSpeed > 0) {
+//for all other cases, change the gear accordingly
+        if (newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
         }
-        if (newSpeed>=1 && newSpeed <= 50){
+        if (newSpeed >= 1 && newSpeed <= 50) {
             this.setGears(1);
-
-        }
-       else if(newSpeed >= 51 && newSpeed <= 100){
+        } else if (newSpeed >= 51 && newSpeed <= 100) {
             this.setGears(2);
-        }
-        else if (newSpeed >= 101 && newSpeed <= 150){
+        } else if (newSpeed >= 101 && newSpeed <= 150) {
             this.setGears(3);
-        } else if (newSpeed>=151 && newSpeed<=200) {
+        } else if (newSpeed >= 151 && newSpeed <= 200) {
             this.setGears(4);
-        } else if (newSpeed>=201 && newSpeed<=250) {
+        } else if (newSpeed >= 201 && newSpeed <= 250) {
             this.setGears(5);
-
-        } else if (newSpeed>250) {
+        } else if (newSpeed > 250) {
             this.setGears(6);
-
         }
-
-
     }
 }
--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -1,24 +1,20 @@
 package com.driver;
-
 public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
-class Boat implements WaterVehicle{
+class Boat implements WaterVehicle {
     private String name;
     private int capacity;
-    public Boat(String name,int capacity){
+    public Boat(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
     }
-
-    @java.lang.Override
+    @Override
     public String getVehicleName() {
         return this.name;
     }
-
-    @java.lang.Override
+    @Override
     public int getVehicleCapacity() {
         return this.capacity;
-    }
-}
+    } }
